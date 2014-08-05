974778f25456ccd14fee979bbed7df7ac2b9b2c6
Pntbr <pntbr@scopyleft.fr>
2014-08-05 16:48:42 +0200

    Add pwgen package

---
+++ packages/app-pwgen/package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Password generator pairing with service"
+});
+
+Package.on_use(function (api, where) {  
+  api.add_files(['lib/client/pwgen.js'], 'client');
+
+  if (api.export)
+    api.export(['Pwgen']);
+});
+
+Package.on_test(function(api) {
+  api.use('app-pwgen', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');  
+
+  api.add_files('pwgen_test.js', 'client');
+});
