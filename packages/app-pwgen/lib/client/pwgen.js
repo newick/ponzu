13f172612d084c8d2a78cffd9c487183e08007dd
pointbar <stephane.langlois@scopyleft.fr>
2015-01-09 18:05:54 +0100

    Add 2nd method

--- packages/app-pwgen/lib/client/pwgen.js
+++ packages/app-pwgen/lib/client/pwgen.js
@@ -1,34 +1,45 @@
 Pwgen = function (sauce, password, service) {
   this.sauce = sauce;
-  this.password = password; 
+  this.password = password;
   this.service = service;
-  
+
   this.methods = {
-    terryaki: this.simpleMeth,
+    terryaki: this.teriyakiMeth,
+    soy: this.soyMeth,
   };
 }
 
 Pwgen.prototype.isSauce = function () {
   var self = this;
-  
+
   if (_.indexOf(_.keys(self.methods), self.sauce) === -1)
     return false;
-  
+
   return true;
 };
 
 Pwgen.prototype.gen = function () {
   var self = this;
-  
+
   return self.methods[self.sauce](this);
 }
 
-Pwgen.prototype.simpleMeth = function (self) {
+Pwgen.prototype.teriyakiMeth = function (self) {
   var password = [];
-  
+
   password.push(self.service.substr(-2));
   password.push(self.password);
   password.push(self.service.substr(0, 2));
-  
+
+  return password.join('');
+};
+
+Pwgen.prototype.soyMeth = function (self) {
+  var password = [];
+
+  password.push(self.service.substr(0, 2));
+  password.push(self.password);
+  password.push(self.service.substr(-2));
+
   return password.join('');
 };
