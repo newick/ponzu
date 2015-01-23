3a9271671cc7debcad86893f9739e1d2d1fedab2
pointbar <stephane.langlois@scopyleft.fr>
2015-01-23 17:40:27 +0100

    Remove logic password code

--- client/passwords.js
+++
@@ -1,9 +0,0 @@
-Template.tplPasswords.helpers({
-  context: function () {
-    return {
-      sessionName: "indexPassword",
-      class: "currentPassword",
-      choices: ["qwerty", "azerty", "grostrol"]
-    };
-  }
-});
